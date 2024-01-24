bf9be380e2e0f2a81b1b6ec31394c97d9d87433e
Desislav Arashev <darashev@cst-bg.net>
2024-01-24 17:02:56 +0200

    Add access_token_create function to create.py

---
+++ ckanext/keycloak_access_token/auth/create.py
@@ -0,0 +1,5 @@
+def access_token_create(context, data_dict):
+    """Create new token for current user.
+    """
+    user = context['model'].User.get(data_dict['user'])
+    return {'success': user.name == context['user']}

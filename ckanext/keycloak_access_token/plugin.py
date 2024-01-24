719ed7e7de80825f358d3bfd95db836627439f57
Desislav Arashev <darashev@cst-bg.net>
2024-01-24 17:02:42 +0200

    Add access_token_create auth function

--- ckanext/keycloak_access_token/plugin.py
+++ ckanext/keycloak_access_token/plugin.py
@@ -2,10 +2,19 @@ import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 import ckanext.keycloak_access_token.blueprint as blueprint
 
+from ckanext.keycloak_access_token.auth.create import access_token_create
+
 
 class KeycloakAccessTokenPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
     plugins.implements(plugins.IBlueprint)
+    plugins.implements(plugins.IAuthFunctions)
+
+    # IAuthFunctions
+    def get_auth_functions(self):
+        return {
+            'access_token_create': access_token_create
+        }
 
     # IConfigurer
     def update_config(self, config_):

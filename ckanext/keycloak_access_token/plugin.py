a2a9d89b47661c54af5ef0c112d1cdf7917529b7
Desislav Arashev <darashev@cst-bg.net>
2024-01-24 17:02:18 +0200

    Add IBlueprint implementation to KeycloakAccessTokenPlugin

--- ckanext/keycloak_access_token/plugin.py
+++ ckanext/keycloak_access_token/plugin.py
@@ -1,14 +1,17 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+import ckanext.keycloak_access_token.blueprint as blueprint
 
 
 class KeycloakAccessTokenPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IBlueprint)
 
     # IConfigurer
-
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
-        toolkit.add_resource('fanstatic',
-            'keycloak_access_token')
+
+    # IBlueprint
+    def get_blueprint(self):
+        return blueprint.keycloak_access_token
